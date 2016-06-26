30726339b7233a9c64a2558051e9c9877219c26c
JillTsai <tsaijdjill@qq.com>
2016-06-26 15:49:12 +0800

    微博

--- resources/views/weibo/weibo.blade.php
+++ resources/views/weibo/weibo.blade.php
@@ -87,12 +87,74 @@
 
             <div class="row">
                 <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                    @if($haveWeibo==0)
-                        <div>
-                            <p>暂无文章</p>
-                        </div>
-                    @else
+
+
+                    {{--<article class="weibo">--}}
+                        {{--<div class="row weibo-head">--}}
+                            {{--<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8">--}}
+                                {{--<div id="">为什么不能在 EDM 模版中使用 DIV ？</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">--}}
+                                {{--<div class=""  style="text-align:right;">--}}
+                                    {{--<div style="display:inline">155</div>--}}
+                                    {{--<div class="btn-group">--}}
+                                        {{--<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                                            {{--已收藏 <span class="caret"></span>--}}
+                                        {{--</button>--}}
+                                        {{--<ul class="dropdown-menu">--}}
+                                            {{--<li><a href="#">取消收藏</a></li>--}}
+
+                                        {{--</ul>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="weibo-time">--}}
+                            {{--<p>2016.06.24 21:18</p>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="weibo-content">--}}
+                            {{--<p>CSS只可使用 内联样式表 ，如：style="margin:0;"</p>--}}
+                            {{--<p>设计之初遵循： 图上无文本，文本后无底纹 的规则</p>--}}
+                            {{--<p>使用 MailChimp HTML Email CSS Fix 参看下文链接</p>--}}
+                            {{--<p>引入 CSS Reset for HTML Email 参看下文链接</p>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="row weibo-foot">--}}
+                            {{--<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">--}}
+                                {{--<div class="weibo-author"style="display:inline">--}}
+                                    {{--<div style="display:inline">王小明</div>--}}
+                                    {{--<div class="btn-group">--}}
+                                        {{--<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                                            {{--已关注 <span class="caret"></span>--}}
+                                        {{--</button>--}}
+                                        {{--<ul class="dropdown-menu">--}}
+                                            {{--<li><a href="#">取消关注</a></li>--}}
+
+                                        {{--</ul>--}}
+                                    {{--</div>--}}
+
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9"style="text-align:right;">--}}
+                                {{--<div class="weibo-label" style="display:inline;text-align:right;">--}}
+                                    {{--<span class="label label-default">食谱</span>--}}
+                                    {{--<span class="label label-default">健身</span>--}}
+                                    {{--<span class="label label-default">健康建议</span>--}}
+                                {{--</div>--}}
+
+
+                            {{--</div>--}}
+
+                        {{--</div>--}}
+
+                    {{--</article>--}}
+
+
                     @for($i=0;$i<count($weibos);$i++)
+
 {{--                    @foreach($weibos as $weibo)--}}
                         <article class="weibo">
                             <div class="row weibo-head">
@@ -101,7 +163,7 @@
                                 </div>
                                 <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4">
                                     <div class=""  style="text-align:right;">
-                                        <div id="count-{{ $weibos[$i]->id }}" style="display:inline">{{ $weibos[$i]->collect_count }}</div>
+                                        <div style="display:inline">{{ $weibos[$i]->collect_count }}</div>
                                             <div class="btn-group">
                                                 <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"
                                                         aria-haspopup="true" aria-expanded="false" id="collect-{{ $weibos[$i]->id }}">
@@ -114,18 +176,11 @@
                                                 </button>
                                                 <ul class="dropdown-menu">
                                                     <li>
-                                                        <a class="collect" id="{{ 'c'.$weibos[$i]->id }}">
-                                                            @if($collects[$i]==0)
-                                                                收藏文章
-                                                            @else
-                                                                取消收藏
-                                                            @endif
-                                                        </a>
-                                                        {{--@if($collects[$i]==0)--}}
-                                                            {{--<a class="confirmcollect" id="{{ 'c'.$weibos[$i]->id }}">收藏文章</a>--}}
-                                                        {{--@else--}}
-                                                            {{--<a class="cancelcollect" id="{{ 'c'.$weibos[$i]->id }}">取消收藏</a>--}}
-                                                        {{--@endif--}}
+                                                        @if($collects[$i]==0)
+                                                            <a class="confirmcollect" id="{{ 'c'.$weibos[$i]->id }}">收藏文章</a>
+                                                        @else
+                                                            <a class="cancelcollect" id="{{ 'c'.$weibos[$i]->id }}">取消收藏</a>
+                                                        @endif
                                                     </li>
                                                 </ul>
                                             </div>
@@ -146,8 +201,8 @@
                                     <div class="weibo-author"style="display:inline">
                                         <div style="display:inline">{{ $names[$i] }}</div>
                                             <div class="btn-group">
-                                                <button type="button" class="btn btn-default btn-xs dropdown-toggle follow-{{ $weibos[$i]->author_id }}" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
+                                                <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"
+                                                        aria-haspopup="true" aria-expanded="false" id="follow-{{ $weibos[$i]->author_id }}">
                                                     @if($follows[$i]==0)
                                                     关注
                                                     @else
@@ -157,19 +212,11 @@
                                                 </button>
                                                 <ul class="dropdown-menu">
                                                     <li>
-                                                        <a class="follow fmenu-{{ $weibos[$i]->author_id }}" id="{{ 'f'.$weibos[$i]->author_id }}">
-                                                            @if($follows[$i]==0)
-                                                                关注作者
-                                                            @else
-                                                                取消关注
-                                                            @endif
-                                                        </a>
-
-                                                        {{--@if($follows[$i]==0)--}}
-                                                            {{--<a class="confirmfollow" id="{{ 'f'.$weibos[$i]->author_id }}">关注作者</a>--}}
-                                                        {{--@else--}}
-                                                            {{--<a class="cancelfollow" id="{{ 'f'.$weibos[$i]->author_id }}">取消关注</a>--}}
-                                                        {{--@endif--}}
+                                                        @if($follows[$i]==0)
+                                                            <a class="confirmfollow" id="{{ 'f'.$weibos[$i]->author_id }}">关注作者</a>
+                                                        @else
+                                                            <a class="cancelfollow" id="{{ 'f'.$weibos[$i]->author_id }}">取消关注</a>
+                                                        @endif
                                                     </li>
 
                                                 </ul>
@@ -196,7 +243,6 @@
                         </article>
 
                     @endfor
-                    @endif
 
                 </div>
 
@@ -217,8 +263,8 @@
                     <div class="weibo-menu">
                         <p><a href="{{ URL::to('/weibo') }}">最新文章</a></p>
                         <p><a href="{{ URL::to('/weibo/hot') }}">热门文章</a></p>
-                        <p><a href="{{ URL::to('/weibo/follow') }}">我的关注</a></p>
-                        <p><a href="{{ URL::to('/weibo/collect') }}">我的收藏</a></p>
+                        <p><a href="{{ URL::to('/weibo') }}">我的关注</a></p>
+                        <p><a href="{{ URL::to('/weibo') }}">我的收藏</a></p>
 
                     </div>
 
